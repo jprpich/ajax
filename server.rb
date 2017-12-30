73d17570005f55cf8c294eb09c69e5d9ed591e14
Joshua Prpich <joshuaprpich@gmail.com>
2017-12-30 17:00:01 -0500

    ajax

---
+++ server.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'json'
+
+get '/' do
+
+  headers['Access-Control-Allow-Origin'] = '*'
+  content_type :text
+
+
+  names = '{
+    "glossary": {
+        "title": "example glossary",
+    "GlossDiv": {
+            "title": "S",
+      "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+          "SortAs": "SGML",
+          "GlossTerm": "Standard Generalized Markup Language",
+          "Acronym": "SGML",
+          "Abbrev": "ISO 8879:1986",
+          "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+            "GlossSeeAlso": ["GML", "XML"]
+                    },
+          "GlossSee": "markup"
+                }
+            }
+        }
+    }
+}'
+
+
+end
